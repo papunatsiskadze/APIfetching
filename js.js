fcbfdb1134980c1c194631bcee9c16fbb84adb49
papunatsiskadze <papunaciskadze@gmail.com>
2022-06-21 15:53:36 +0400

    fully done

--- js.js
+++ js.js
@@ -5,14 +5,37 @@ getinfo(APIURL)
 async function getinfo (url) {
     const res = await fetch (url)
     const data = await res.json()
+    showinfo(data.results)
 }
 
-function showinfo (monacemebi) {
-    
-    monacemebi.forEach((infos) => {
-        const { gender,name } = infos
+function showinfo (users) {
+    main.innerHTML = ''
+    users.forEach((user) => {
+        const { picture,name,location,dob,phone } = user
+
+        const userEl = document.createElement('div')
+        userEl.classList.add('user')
+
+        userEl.innerHTML = ` 
+        <div class="outterdiv">
+           <img src="${picture.large}">
+        <h4>
+            ${name.title + ' ' + name.first + ' ' + name.last}
+        </h4>
+        <p>
+            ${location.city + ' ' + location.street.number + ' ' + location.street.name} 
+        </p>
+        <p>
+            ${dob.age}
+        </p>
+        <p>
+            ${phone}
+        </p>
+       </div>
+        `
+
+        main.appendChild(userEl)
     })
 
 
 }
-document.write(getinfo)
